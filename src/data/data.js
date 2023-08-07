bb7d2ddcd6310325efb96207fe337a11b861140d
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-08-07 21:04:21 +0300

    feat: data to be fetched

---
+++ src/data/data.js
@@ -0,0 +1,33 @@
+const projectData =  [
+    
+          {
+            "id": 1,
+            "itemCode": "67",
+            "item": "Pen Red",
+            "brand": "Bic",
+            "unitPrice": "50",
+            "unitOfMeasurement": "Each",
+            "hasThreshold": "true",
+          },
+          {
+            "id": 2,
+            "itemCode": "257427",
+            "item": "Pen White",
+            "brand": "Pelikan",
+            "unitPrice": "40",
+            "unitOfMeasurement": "Each",
+            "hasThreshold": "true",
+          },
+          {
+            "id": 3,
+            "itemCode": "257427",
+            "item": "Pen Purple",
+            "brand": "Pelikan",
+            "unitPrice": "40",
+            "unitOfMeasurement": "Each",
+            "hasThreshold": "false",
+          }
+
+]
+export default projectData
+
